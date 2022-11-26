8df157cd6d87554434fd5f43f23f84b83a8b772c
Joshua <gildas2526@gmail.com>
2022-11-26 10:51:22 +0100

    (feat) Smoke Test

---
+++ jaagen-exchange/contracts/Token.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+// Uncomment this line to use console.log
+import "hardhat/console.sol";
+
+contract Token {
+    // Uncomment this line, and the import of "hardhat/console.sol", to print a log in your terminal
+    // console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
+    string public name = "Strella Coin";
+}
--- jaagen-exchange/hardhat.config.js
+++ jaagen-exchange/hardhat.config.js
@@ -2,5 +2,8 @@ require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17"
+  solidity: "0.8.17",
+  networks: {
+    localhost: {} //it will understand automatically to connect to localhost
+  }
 };
