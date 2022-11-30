24106ea98c98038de2f22977a6a39f763d9bdc9b
Joshua <gildas2526@gmail.com>
2022-11-30 01:09:54 +0100

    (feat) create exchange track fee account

---
+++ jaagen-exchange/test/Exchange.js
@@ -0,0 +1,29 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+const tokens = n => {
+  return ethers.utils.parseUnits(n.toString(), "ether");
+};
+
+describe("Exchange", () => {
+  let exchange, accounts, deployer, feeAccount;
+  const feePercent = 10;
+
+  beforeEach(async () => {
+    accounts = await ethers.getSigners();
+    deployer = accounts[0];
+    feeAccount = accounts[1];
+
+    const Exchange = await ethers.getContractFactory("Exchange");
+    exchange = await Exchange.deploy(feeAccount.address, feePercent);
+  });
+
+  describe("Deployment", () => {
+    it("Tracks the fee account", async () => {
+      expect(await exchange.feeAccount()).to.equal(feeAccount.address);
+    });
+    it("Tracks the fee percent", async () => {
+      expect(await exchange.feePercent()).to.equal(feePercent);
+    });
+  });
+});
