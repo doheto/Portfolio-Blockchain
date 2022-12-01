a1062bb209fc70548ce184a61455d5e0cdc1636f
Joshua <gildas2526@gmail.com>
2022-12-01 16:53:03 +0100

    (feat) making orders on exchange

--- jaagen-exchange/test/Exchange.js
+++ jaagen-exchange/test/Exchange.js
@@ -170,4 +170,61 @@ describe("Exchange", () => {
       );
     });
   });
+
+  describe("Making orders", async () => {
+    let transaction, result;
+
+    let amount = tokens(1);
+
+    describe("Success", async () => {
+      beforeEach(async () => {
+        // Deposit tokens before making order
+
+        // Approve Token
+        transaction = await token1
+          .connect(user1)
+          .approve(exchange.address, amount);
+        result = await transaction.wait();
+        // Deposit token
+        transaction = await exchange
+          .connect(user1)
+          .depositToken(token1.address, amount);
+        result = await transaction.wait();
+
+        // Make order
+        transaction = await exchange
+          .connect(user1)
+          .makeOrder(token2.address, amount, token1.address, amount);
+        result = await transaction.wait();
+      });
+
+      it("tracks the newly created order", async () => {
+        expect(await exchange.orderCount()).to.equal(1);
+      });
+
+      it("emits an Order event", async () => {
+        const event = result.events[0];
+        expect(event.event).to.equal("Order");
+
+        const args = event.args;
+        expect(args.id).to.equal(1);
+        expect(args.user).to.equal(user1.address);
+        expect(args.tokenGet).to.equal(token2.address);
+        expect(args.amountGet).to.equal(tokens(1));
+        expect(args.tokenGive).to.equal(token1.address);
+        expect(args.amountGive).to.equal(tokens(1));
+        expect(args.timestamp).to.at.least(1);
+      });
+    });
+
+    describe("Failure", async () => {
+      it("Rejects with no balance", async () => {
+        await expect(
+          exchange
+            .connect(user1)
+            .makeOrder(token2.address, tokens(1), token1.address, tokens(1))
+        ).to.be.reverted;
+      });
+    });
+  });
 });
