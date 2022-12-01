b40f1ac1c5094a498486c38525a26e385c9cacd8
Joshua <gildas2526@gmail.com>
2022-12-01 17:35:51 +0100

    (feat) Cancel Orders

--- jaagen-exchange/contracts/Exchange.sol
+++ jaagen-exchange/contracts/Exchange.sol
@@ -9,6 +9,8 @@ contract Exchange {
     address public feeAccount;
     uint256 public feePercent;
     uint256 public orderCount;
+
+
     //balance
     //token address           user address  tokens
     mapping(address => mapping(address => uint256)) public tokens;
@@ -16,7 +18,7 @@ contract Exchange {
     mapping(uint256 => _Order) public orders;
     // Modeling an order
     struct _Order {
-        // Attributes of an order 
+        // Attributes of an order
         uint256 id; //unique id for order
         address user; // User who made order
         address tokenGet; // Address of the token they receive
@@ -25,6 +27,9 @@ contract Exchange {
         uint256 amountGive; // Amount they give
         uint256 timeStamp;
     }
+    // Cancelled orders
+    mapping(uint256 => bool) public orderCancelled;
+
 
     event Deposit(address token, address user, uint256 amount, uint256 balance);
     event Withdraw(
@@ -42,6 +47,15 @@ contract Exchange {
         uint256 amountGive,
         uint256 timestamp
     );
+    event Cancel(
+        uint256 id,
+        address user,
+        address tokenGet,
+        uint256 amountGet,
+        address tokenGive,
+        uint256 amountGive,
+        uint256 timestamp
+    );
 
     constructor(
         address _feeAccount,
@@ -50,6 +64,8 @@ contract Exchange {
         feeAccount = _feeAccount;
         feePercent = _feePercent;
     }
+
+
     // Deposit Tokens
     function depositToken(
         address _token,
@@ -126,5 +142,31 @@ contract Exchange {
             _amountGive,
             block.timestamp
         );
+    }
+
+    function cancelOrder(uint256 _id) public {
+        // Fetch order
+        _Order storage _order = orders[_id];
+
+        // Ensure the caller of the function is the owner of the order
+        require(address(_order.user) == msg.sender, "caller of the function is not the owner of the order");
+
+        // Order must exist
+        require(_order.id == _id, "Order doesn't exist");
+
+        // Cancel the order
+        orderCancelled[_id] = true;
+
+        // Emit event
+        emit Cancel(
+            _order.id,
+            msg.sender,
+            _order.tokenGet,
+            _order.amountGet,
+            _order.tokenGive,
+            _order.amountGive,
+            block.timestamp
+        );
+
     }
 }
