4dea0aba1901075129fe240fb3d2d998f25afe57
Joshua <gildas2526@gmail.com>
2022-12-14 02:33:02 +0100

    smartcontract deployed to infura and website to vercel or fleek

--- jaagen-exchange/hardhat.config.js
+++ jaagen-exchange/hardhat.config.js
@@ -1,9 +1,14 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
+const privateKeys = process.env.PRIVATE_KEYS || ""; //if no private keys just return empty
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
   networks: {
-    localhost: {} //it will understand automatically to connect to localhost
+    localhost: {}, //it will understand automatically to connect to localhost
+    goerli: {
+      url: `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: privateKeys.split(",")
+    }
   }
 };
