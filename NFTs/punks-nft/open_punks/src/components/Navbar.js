af5a6793d0bde3c380d3820897c83634fe15594d
Joshua <gildas2526@gmail.com>
2022-12-22 23:58:37 +0100

    (feat) nft improvements contracts

--- NFTs/punks-nft/open_punks/src/components/Navbar.js
+++ NFTs/punks-nft/open_punks/src/components/Navbar.js
@@ -1,31 +1,34 @@
-import logo from '../images/logo.png'
+import logo from "../images/logo.png";
 
 const Navbar = ({ web3Handler, account, explorerURL }) => {
-    return (
-        <nav className="navbar fixed-top mx-3">
-            <a
-                className="navbar-brand col-sm-3 col-md-2 mr-0 mx-4"
-                href="http://www.dappuniversity.com/bootcamp"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                <img src={logo} className="App-logo" alt="logo" />
-                Dapp University
-            </a>
+  return (
+    <nav className="navbar fixed-top mx-3">
+      <a
+        className="navbar-brand col-sm-3 col-md-2 mr-0 mx-4"
+        href="http://www.joshua.gildas.tech"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        Das NFT
+      </a>
 
-            {account ? (
-                <a
-                    href={`${explorerURL}/address/${account}`}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="button nav-button btn-sm mx-4">
-                    {account.slice(0, 5) + '...' + account.slice(38, 42)}
-                </a>
-            ) : (
-                <button onClick={web3Handler} className="button nav-button btn-sm mx-4">Connect Wallet</button>
-            )}
-        </nav>
-    )
-}
+      {account ? (
+        <a
+          href={`${explorerURL}/address/${account}`}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="button nav-button btn-sm mx-4"
+        >
+          {account.slice(0, 5) + "..." + account.slice(38, 42)}
+        </a>
+      ) : (
+        <button onClick={web3Handler} className="button nav-button btn-sm mx-4">
+          Connect Wallet
+        </button>
+      )}
+    </nav>
+  );
+};
 
 export default Navbar;
