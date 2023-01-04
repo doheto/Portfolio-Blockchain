20363fef847532db9fdd24b214b42ac7e403bd4a
Joshua <gildas2526@gmail.com>
2023-01-04 23:57:39 +0100

    update

--- YieldFarming/juicyswap/truffle-config.js
+++ YieldFarming/juicyswap/truffle-config.js
@@ -1,8 +1,8 @@
-require('babel-register');
-require('babel-polyfill');
-require('dotenv').config();
-const HDWalletProvider = require('truffle-hdwallet-provider-privkey');
-const privateKeys = process.env.PRIVATE_KEYS || ""
+require("babel-register");
+require("babel-polyfill");
+require("dotenv").config();
+const HDWalletProvider = require("truffle-hdwallet-provider-privkey");
+const privateKeys = process.env.PRIVATE_KEYS || "";
 
 module.exports = {
   networks: {
@@ -14,18 +14,37 @@ module.exports = {
     rinkeby: {
       provider: function() {
         return new HDWalletProvider(
-          privateKeys.split(','), // Array of account private keys
-          `https://rinkeby.infura.io/v3/${process.env.INFURA_API_KEY}`// Url to an Ethereum Node
-        )
+          privateKeys.split(","), // Array of account private keys
+          `https://rinkeby.infura.io/v3/${process.env.INFURA_API_KEY}` // Url to an Ethereum Node
+        );
       },
       gas: 5000000,
       gasPrice: 25000000000,
       network_id: 4
+    },
+    goerli: {
+      provider: function() {
+        return new HDWalletProvider(
+          [process.env.DEPLOYER_PRIVATE_KEY],
+          `wss://goerli.infura.io/ws/v3/${process.env.INFURA_API_KEY}` // URL to Ethereum Node
+        );
+      },
+      gasPrice: 60000000000, // 60 Gwei
+      gas: 4000000,
+      network_id: 5
+    },
+
+    matic: {
+      provider: function() {
+        return new HDWalletProvider(
+          [process.env.DEPLOYER_PRIVATE_KEY],
+          `https://polygon-rpc.com`
+        );
+      },
+      network_id: 137
     }
   },
-  plugins: [
-    'truffle-plugin-verify'
-  ],
+  plugins: ["truffle-plugin-verify"],
   api_keys: {
     etherscan: process.env.ETHERSCAN_API_KEY
   },
