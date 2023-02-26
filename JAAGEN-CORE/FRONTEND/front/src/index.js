93ec621285ae0df31e2eae0f0f00e3afc0a1348c
Joshua <gildas2526@gmail.com>
2023-02-27 00:34:13 +0100

    adding all elements to compile

---
+++ JAAGEN-CORE/FRONTEND/front/src/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./_store";
+import { App } from "App";
+import "antd/dist/antd.css";
+import "./index.css";
+import registerServiceWorker from "./registerServiceWorker";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+registerServiceWorker();
